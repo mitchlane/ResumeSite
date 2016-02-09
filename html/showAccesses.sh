7851d57732d02691b0e9fe3078e4e2627e1e7d0e
mitchlane <mitchlane44@gmail.com>
2016-02-08 16:43:08 -0800

    showAccesses updated

--- html/showAccesses.sh
+++ html/showAccesses.sh
@@ -1,2 +1,5 @@
-awk '{ print $1 } ' /var/log/apache2/access.log | sort | uniq
+awk '{ print $1 } ' /var/log/apache2/access.log | sort | uniq > new.txt
+diff old.txt new.txt
 awk '{ print $1 } ' /var/log/apache2/access.log | sort | uniq | wc -l
+awk '{ print $1 } ' /var/log/apache2/access.log | sort | uniq > old.txt
+rm new.txt
